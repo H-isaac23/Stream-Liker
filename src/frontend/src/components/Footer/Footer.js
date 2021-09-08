005f852c25258fb379d92b70648887eb60a0c959
H-isaac23 <dev.isaac23@gmail.com>
2021-09-08 13:22:58 +0800

    Style footer

--- src/frontend/src/components/Footer/Footer.js
+++ src/frontend/src/components/Footer/Footer.js
@@ -6,23 +6,25 @@ const Footer = () => {
   const github_site = "https://github.com/H-isaac23";
 
   return (
-    <div className={styles.footerContainer}>
-      <p className={styles.projectName}>"Stream Liker" Project</p>
-      <p className={styles.socials}>
-        <a href={github_site} target="_blank" rel="noreferrer">
-          Github
-        </a>
-        <a href={twitter_site} target="_blank" rel="noreferrer">
-          Twitter
-        </a>
-        <a href={reddit_site} target="_blank" rel="noreferrer">
-          Reddit
-        </a>
-      </p>
-      <div className={styles.c}>
-        <p>© 2021 Stream-Liker</p>
+    <footer className={styles.footer}>
+      <div className={styles.footerContainer}>
+        <p className={styles.projectName}>"Stream Liker" Project</p>
+        <p className={styles.socials}>
+          <a href={github_site} target="_blank" rel="noreferrer">
+            Github
+          </a>
+          <a href={twitter_site} target="_blank" rel="noreferrer">
+            Twitter
+          </a>
+          <a href={reddit_site} target="_blank" rel="noreferrer">
+            Reddit
+          </a>
+        </p>
+        <div className={styles.c}>
+          <p>© 2021 Stream-Liker</p>
+        </div>
       </div>
-    </div>
+    </footer>
   );
 };
 
