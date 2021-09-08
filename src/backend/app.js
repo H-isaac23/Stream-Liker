f8b259ada78291b0d1d47b44bfc7ec1ae5b8dafc
H-isaac23 <dev.isaac23@gmail.com>
2021-09-08 12:38:31 +0800

    setup all middlewares

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,1 +1,27 @@
-require("dotenv").config();
+require("express-async-errors");
+const express = require("express");
+const app = express();
+const middleware = require("./utils/middleware");
+const mongoose = require("mongoose");
+const config = require("./utils/config");
+const url = config.MONGODB_URI;
+const accountRouter = require("./controllers/accounts");
+const cors = require("cors");
+
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err.message));
+
+app.use(cors());
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+app.use("/api/v1/accounts", accountRouter);
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
