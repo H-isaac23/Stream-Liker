c77075d3ca3ea33b2792fbf94244a4ed40744da1
H-isaac23 <dev.isaac23@gmail.com>
2021-09-08 12:38:43 +0800

    setup server

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,1 +1,10 @@
 const http = require("http");
+const app = require("./app");
+const config = require("./utils/config");
+
+const server = http.createServer(app);
+const PORT = config.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log("Server listening to PORT", PORT);
+});
