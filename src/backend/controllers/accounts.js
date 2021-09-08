6c37843d5fd561057711c30f6c29db4ef2672a5d
H-isaac23 <dev.isaac23@gmail.com>
2021-09-08 12:08:41 +0800

    add post req

--- src/backend/controllers/accounts.js
+++ src/backend/controllers/accounts.js
@@ -6,4 +6,13 @@ accountRouter.get("/", async (req, res) => {
   res.json(accounts);
 });
 
-accountRouter.post("/", async (req, res) => {});
+accountRouter.post("/", async (req, res) => {
+  const body = req.body;
+  const newAccount = new Account({
+    accountName: body.accountName,
+    accountId: body.accoundId,
+  });
+
+  await newAccount.save();
+  res.status.end();
+});
