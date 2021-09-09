06cfe7e623ded5b4dafdda78c8eedae75347ccc7
H-isaac23 <dev.isaac23@gmail.com>
2021-09-09 18:11:10 +0800

    rename import

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,5 +1,5 @@
 import Footer from "./components/Footer/Footer";
-import Body from "./components/LikeButton/Body";
+import Body from "./components/Body/Body";
 import Header from "./components/Header/Header";
 import "./styles/globals.scss";
 
