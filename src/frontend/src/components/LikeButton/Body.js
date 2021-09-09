02c05dc355978c6d5f494d57c00054d1d5bfda68
H-isaac23 <dev.isaac23@gmail.com>
2021-09-09 18:07:43 +0800

    add animation on like to loading sequence

--- src/frontend/src/components/LikeButton/Body.js
+++ src/frontend/src/components/LikeButton/Body.js
@@ -1,29 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Body.module.scss";
 import Loader from "react-loader-spinner";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import { CSSTransition } from "react-transition-group";
+import "./Body.css";
 
 const Body = () => {
+  const [showing, setShowing] = useState("Like");
+
+  const onClickingLike = () => {
+    setShowing("Loading");
+  };
+
   return (
     <div className={styles.bodyContainer}>
-      <LikeButton />
-      <div className={styles.loaderContainer}>
-        <Loader
-          type="BallTriangle"
-          color="#00BFFF"
-          height={80}
-          width={80}
-          className={styles.loader}
-        />
-      </div>
+      <CSSTransition
+        in={showing === "Like"}
+        unmountOnExit
+        timeout={1000}
+        classNames="body-primary"
+      >
+        <LikeButton onClick={onClickingLike} />
+      </CSSTransition>
+      <CSSTransition
+        in={showing === "Loading"}
+        unmountOnExit
+        timeout={1000}
+        classNames="bodyLoading"
+      >
+        <div className={styles.loaderContainer}>
+          <Loader type="BallTriangle" color="#00BFFF" height={80} width={80} />
+        </div>
+      </CSSTransition>
     </div>
   );
 };
 
-const LikeButton = () => {
+const LikeButton = ({ onClick }) => {
   return (
     <div className={styles.buttonContainer}>
-      <button>Like</button>
+      <button onClick={onClick}>Like</button>
     </div>
   );
 };
