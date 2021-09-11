f54d380c35b01b4cb3cd0fb2b2f38a706b2c839c
H-isaac23 <dev.isaac23@gmail.com>
2021-09-11 23:00:49 +0800

    finish get active streams func

--- src/frontend/src/services/getStreams.js
+++ src/frontend/src/services/getStreams.js
@@ -1,48 +1,30 @@
 const axios = require("axios");
 const baseUrl =
-  "https://protected-coast-46250.herokuapp.com/https://www.youtube.com/channel/";
+  process.env.REACT_APP_CORS_ANYWHERE_URL + "https://www.youtube.com/channel/";
 
 const getStreams = async (accounts) => {
   const promiseArray = accounts.map(async (account) => {
-    const response = await axios.get(baseUrl + account.accountId);
-    return response;
-  });
-  console.log(promiseArray);
-  console.time("do this");
-
-  const streamers = await Promise.all(promiseArray);
-
-  console.timeEnd("do this");
-  console.log(streamers);
-
-  const streamsData = streamers.filter(
-    // eslint-disable-next-line quotes
-    (stream) => stream.data.search('{"text":" watching"}') > 0
-  );
-
-  streamsData.forEach((stream) => {
-    for (let i = 0; i < accounts.length; i++) {
-      if (accounts[i].accountId === stream.config.url.slice(32)) {
-        // eslint-disable-next-line quotes
-        const index = stream.data.search('{"text":" watching"}');
-        const log = stream.data.slice(index);
-        // eslint-disable-next-line quotes
-        const url_index = log.indexOf('{"url":"/watch?v=');
-        accounts[i].streamId = log.slice(url_index + 17, url_index + 28);
-        break;
-      }
+    const res = await axios.get(baseUrl + account.accountId);
+    const streamer = { ...account };
+    const index = res.data.search('{"text":" watching"}');
+    if (index > 0) {
+      const sliced = res.data.slice(index);
+      const url = sliced.search('"url"');
+      streamer.streamUrl = "youtube.com" + sliced.slice(url + 7, url + 27);
     }
+    return streamer;
   });
 
-  const streams = streamsData.map((stream) => stream.config.url.slice(32));
-
-  const activeStreamers = accounts.filter((account) =>
-    streams.includes(account.accountId)
+  const response = await Promise.all(promiseArray);
+  const activeStreams = response.filter((streamer) =>
+    streamer.hasOwnProperty("streamUrl")
   );
 
-  console.log(activeStreamers, "hi");
-
-  // return { data: activeStreamers };
+  if (activeStreams.length >= 1) {
+    return activeStreams;
+  } else {
+    return ["No Streams Available"];
+  }
 };
 
 // getStreams(accounts);
