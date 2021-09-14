53ed91d85682e4159e4eba3bb89824487ad3643f
H-isaac23 <dev.isaac23@gmail.com>
2021-09-14 20:33:03 +0800

    initial liked streams setup

---
+++ src/frontend/src/components/Streams/LikedStreams.js
@@ -0,0 +1,11 @@
+import styles from "./LikedStreams.module.scss";
+
+const LikedStreams = () => {
+  return (
+    <div>
+      <div className={styles.container}>boilerplate</div>
+    </div>
+  );
+};
+
+export default LikedStreams;
