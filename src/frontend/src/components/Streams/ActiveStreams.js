48baea67a52337773dbe3364fae3fef87e72852d
H-isaac23 <dev.isaac23@gmail.com>
2021-09-14 20:31:24 +0800

    initial active streams setup

---
+++ src/frontend/src/components/Streams/ActiveStreams.js
@@ -0,0 +1,11 @@
+import styles from "./ActiveStreams.module.scss";
+
+const ActiveStreams = () => {
+  return (
+    <div>
+      <div className={styles.container}>boiler plate</div>
+    </div>
+  );
+};
+
+export default ActiveStreams;
