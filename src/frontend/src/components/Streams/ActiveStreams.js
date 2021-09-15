14bf4e261da837ddb9632163dd4383527c58ef20
H-isaac23 <dev.isaac23@gmail.com>
2021-09-15 16:14:06 +0800

    complete active stream template

--- src/frontend/src/components/Streams/ActiveStreams.js
+++ src/frontend/src/components/Streams/ActiveStreams.js
@@ -1,9 +1,11 @@
 import styles from "./ActiveStreams.module.scss";
 
-const ActiveStreams = () => {
+const ActiveStreams = ({ name, url }) => {
   return (
-    <div>
-      <div className={styles.container}>boiler plate</div>
+    <div className={styles.streamContainer}>
+      <a href={url} target="_blank" rel="noreferrer">
+        <div className={styles.container}>{name}</div>
+      </a>
     </div>
   );
 };
