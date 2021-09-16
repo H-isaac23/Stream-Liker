99571120c7992d9ee13bc8a4e6520ee88e322925
H-isaac23 <dev.isaac23@gmail.com>
2021-09-16 16:44:43 +0800

    wrap everything into videoLiker func

--- src/backend/api/oauth2.js
+++ src/backend/api/oauth2.js
@@ -6,106 +6,114 @@ const fs = require("fs");
 const { google } = require("googleapis");
 const destroyer = require("server-destroy");
 
-const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const REDIRECT_URL = process.env.REDIRECT_URI;
-const oauth2Client = new google.auth.OAuth2(
-  CLIENT_ID,
-  CLIENT_SECRET,
-  REDIRECT_URL
-);
+const videoLiker = (streams) => {
+  const CLIENT_ID = process.env.CLIENT_ID;
+  const CLIENT_SECRET = process.env.CLIENT_SECRET;
+  const REDIRECT_URL = process.env.REDIRECT_URI;
+  const oauth2Client = new google.auth.OAuth2(
+    CLIENT_ID,
+    CLIENT_SECRET,
+    REDIRECT_URL
+  );
 
-google.options({ auth: oauth2Client });
-const scope = ["https://www.googleapis.com/auth/youtube"];
-const authenticate = async () => {
-  return new Promise((resolve, reject) => {
-    const authUrl = oauth2Client.generateAuthUrl({
-      access_type: "offline",
-      scope: scope,
-    });
+  const videoUrls = streams.map((stream) => stream.streamUrl);
 
-    const server = http.createServer(async (req, res) => {
-      try {
-        if (req.url.indexOf("/oauth2callback" > -1)) {
-          const qs = new url.URL(req.url, "http://localhost:5000").searchParams;
-          res.end("Authentication successful! Please return to the console.");
-          server.destroy();
-          const { tokens } = await oauth2Client.getToken(qs.get("code"));
-          oauth2Client.credential = tokens;
-          resolve(oauth2Client);
+  google.options({ auth: oauth2Client });
+  const scope = ["https://www.googleapis.com/auth/youtube"];
+  const authenticate = async () => {
+    return new Promise((resolve, reject) => {
+      const authUrl = oauth2Client.generateAuthUrl({
+        access_type: "offline",
+        scope: scope,
+      });
+
+      const server = http.createServer(async (req, res) => {
+        try {
+          if (req.url.indexOf("/oauth2callback" > -1)) {
+            const qs = new url.URL(req.url, "http://localhost:5000")
+              .searchParams;
+            res.end("Authentication successful! Please return to the console.");
+            server.destroy();
+            const { tokens } = await oauth2Client.getToken(qs.get("code"));
+            oauth2Client.credential = tokens;
+            resolve(oauth2Client);
+          }
+        } catch (e) {
+          reject(e);
         }
-      } catch (e) {
-        reject(e);
-      }
+      });
+
+      server.listen(5000, () => {
+        opn(authUrl, { wait: false }).then((cp) => cp.unref());
+      });
+
+      destroyer(server);
     });
+  };
 
-    server.listen(5000, () => {
-      opn(authUrl, { wait: false }).then((cp) => cp.unref());
-    });
+  let credjson = fs.readFileSync("credentials.json", "utf-8");
+  let credentials = JSON.parse(credjson);
+  console.log(credentials.length === 0);
 
-    destroyer(server);
-  });
-};
-
-let credjson = fs.readFileSync("credentials.json", "utf-8");
-let credentials = JSON.parse(credjson);
-console.log(credentials.length === 0);
-
-const likeVideo = (auth, v_id) => {
-  console.log("Liking video...");
-  const service = google.youtube("v3");
-  service.playlistItems.insert(
-    {
-      auth: auth,
-      part: "snippet",
-      requestBody: {
-        snippet: {
-          playlistId: "LL",
-          resourceId: {
-            kind: "youtube#video",
-            videoId: v_id,
+  const likeVideo = (auth, v_id) => {
+    console.log("Liking video...");
+    const service = google.youtube("v3");
+    service.playlistItems.insert(
+      {
+        auth: auth,
+        part: "snippet",
+        requestBody: {
+          snippet: {
+            playlistId: "LL",
+            resourceId: {
+              kind: "youtube#video",
+              videoId: v_id,
+            },
           },
         },
       },
-    },
-    function (err) {
-      if (err) {
-        console.log(err.errors[0].message);
-        return;
-      }
+      function (err) {
+        if (err) {
+          console.log(err.errors[0].message);
+          return;
+        }
 
-      console.log("Liked video");
+        console.log("Liked video");
+      }
+    );
+  };
+
+  if (credentials.length === 0 || JSON.stringify(credentials)[0] === "{}") {
+    authenticate().then((client) => {
+      credentials = [client.credential];
+      credjson = JSON.stringify(credentials);
+      fs.writeFileSync("credentials.json", credjson, "utf-8");
+      console.log(credentials, "=====================");
+      for (let i = 0; i < videoUrls.length; i++) {
+        likeVideo(oauth2Client, videoUrls[i]);
+      }
+    });
+  } else {
+    const tokens = credentials[0];
+    oauth2Client.setCredentials(tokens);
+    oauth2Client.on("tokens", (tokens) => {
+      if (tokens.refresh_token) {
+        console.log(tokens.refresh_token);
+      }
+      console.log(tokens.access_token);
+    });
+
+    for (let i = 0; i < videoUrls.length; i++) {
+      likeVideo(oauth2Client, videoUrls[i]);
     }
-  );
+
+    // const testArray = [
+    //   "LL9wopVDO1g",
+    //   "Zz8PJpu5Sgw",
+    //   "aPjeUUE25bQ",
+    //   "JVWqmb15QGI",
+    // ];
+  }
 };
 
-if (credentials.length === 0 || JSON.stringify(credentials)[0] === "{}") {
-  authenticate().then((client) => {
-    credentials = [client.credential];
-    credjson = JSON.stringify(credentials);
-    fs.writeFileSync("credentials.json", credjson, "utf-8");
-    console.log(credentials, "=====================");
-    likeVideo();
-  });
-} else {
-  const tokens = credentials[0];
-  oauth2Client.setCredentials(tokens);
-  oauth2Client.on("tokens", (tokens) => {
-    if (tokens.refresh_token) {
-      console.log(tokens.refresh_token);
-    }
-    console.log(tokens.access_token);
-  });
-
-  // const testArray = [
-  //   "LL9wopVDO1g",
-  //   "Zz8PJpu5Sgw",
-  //   "aPjeUUE25bQ",
-  //   "JVWqmb15QGI",
-  // ];
-  // for (let i = 0; i < testArray.length; i++) {
-  //   likeVideo(oauth2Client, testArray[i]);
-  // }
-
-  // setTimeout(likeVideo(oauth2Client, "Zz8PJpu5Sgw"), 5000);
-}
+module.exports = videoLiker;
