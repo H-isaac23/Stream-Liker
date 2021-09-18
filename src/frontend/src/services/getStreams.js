aa58bef7a99ac0ffba2f281635b4810ba2374b24
H-isaac23 <dev.isaac23@gmail.com>
2021-09-18 14:56:50 +0800

    fix mobile getStreams

--- src/frontend/src/services/getStreams.js
+++ src/frontend/src/services/getStreams.js
@@ -4,26 +4,29 @@ const baseUrl =
 const apiUrl = "/api/v1/accounts";
 
 const getStreams = async (accounts, isMobile) => {
-  console.log(isMobile);
   const textSearch = isMobile ? "watching" : '{"text":" watching"}';
+  const urlSearch = isMobile ? "videoId" : '"url"';
 
   const promiseArray = accounts.map(async (account) => {
     const res = await axios.get(baseUrl + account.accountId);
     const streamer = { ...account };
 
     const index = res.data.search(textSearch);
-    if (index > 0) {
-      console.log(streamer);
+
+    if (index > 0 && index < 400000) {
       const sliced = res.data.slice(index);
-      const url = sliced.search('"url"');
-      // streamer.streamUrl = "youtube.com" + sliced.slice(url + 7, url + 27);
-      streamer.streamUrl = "youtube.com";
+      const url = sliced.search(urlSearch);
+      if (isMobile) {
+        streamer.streamUrl =
+          "youtube.com/watch?v=" + sliced.slice(url + 16, url + 27);
+      } else {
+        streamer.streamUrl = "youtube.com" + sliced.slice(url + 7, url + 27);
+      }
     }
     return streamer;
   });
 
   const response = await Promise.all(promiseArray);
-  console.log(response);
   const activeStreams = response.filter((streamer) =>
     streamer.hasOwnProperty("streamUrl")
   );
